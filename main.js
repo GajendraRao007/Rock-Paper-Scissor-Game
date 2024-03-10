be06bb07e362ef25424fa52dc27c5577710547b4
Gajendra <raogajendra27@gmail.com>
2024-03-10 18:04:03 +0530

    js - added the functionality, when the user picks any options(rock,paper,scissor) & displayed on the page

--- main.js
+++ main.js
@@ -13,3 +13,34 @@ cancelButton.addEventListener('click',()=>{
     console.log('cancel button clicked');
     rulesBox.style.display = 'none';
 })
+
+//Picked options
+let pickedRock = document.getElementById('rockbtn');
+let pickedPaper = document.getElementById('paperbtn');
+let pickedScissor = document.getElementById('scissorbtn');
+
+
+
+pickedRock.addEventListener('click',()=>{
+    let displayRock = document.getElementById('you-picked');
+    displayRock.innerHTML = '<img src="Images/Group 6.png" alt="stone" srcset="">';
+    console.log('pickd rock');
+    let options = document.getElementById('options');
+    options.style.display = 'none';
+
+});
+pickedPaper.addEventListener('click',()=>{
+    let displayPaper =document.getElementById('you-picked');
+    displayPaper.innerHTML = '<img src="Images/Group 8.png" alt="paper" srcset="">';
+    console.log('picked paper');
+    let options = document.getElementById('options');
+    options.style.display = 'none';
+})
+
+pickedScissor.addEventListener('click',()=>{
+    let displayScissor = document.getElementById('you-picked');
+    displayScissor.innerHTML ='<img src="Images/Group 7.png" alt="scissor" srcset="">';
+    console.log('picked scissor');
+    let options = document.getElementById('options');
+    options.style.display = 'none';
+})
