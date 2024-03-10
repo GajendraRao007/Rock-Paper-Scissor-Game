7d65a77e251283f5123bab9ba3b10b61f57e6019
Gajendra <raogajendra27@gmail.com>
2024-03-10 19:23:26 +0530

    added js for - computer pick Random options(rock,paper,scissor) & displayed on page

--- main.js
+++ main.js
@@ -27,20 +27,46 @@ pickedRock.addEventListener('click',()=>{
     console.log('pickd rock');
     let options = document.getElementById('options');
     options.style.display = 'none';
+    
+    let showRound = document.getElementById('match-area')
+    showRound.style.display = 'flex';
 
 });
 pickedPaper.addEventListener('click',()=>{
     let displayPaper =document.getElementById('you-picked');
     displayPaper.innerHTML = '<img src="Images/Group 8.png" alt="paper" srcset="">';
     console.log('picked paper');
+
     let options = document.getElementById('options');
     options.style.display = 'none';
+
+    let showRound = document.getElementById('match-area')
+    showRound.style.display = 'flex';
 })
 
 pickedScissor.addEventListener('click',()=>{
     let displayScissor = document.getElementById('you-picked');
     displayScissor.innerHTML ='<img src="Images/Group 7.png" alt="scissor" srcset="">';
     console.log('picked scissor');
+
     let options = document.getElementById('options');
     options.style.display = 'none';
+
+    let showRound = document.getElementById('match-area')
+    showRound.style.display = 'flex';
 })
+
+let compicked = ['pickedRock', 'pickedPaper', 'pickedScissor'];
+
+    compickedIndex = Math.floor(Math.random()* compicked.length);
+    
+    let comDisplay = document.getElementById('Pc-picked');
+    let compickedRandom = compicked[compickedIndex];
+
+    if(compickedRandom == 'pickedRock'){
+        comDisplay.innerHTML = '<img src="Images/Group 6.png" alt="stone" srcset="">';
+    }else if(compickedRandom == 'pickedPaper'){
+        comDisplay.innerHTML = '<img src="Images/Group 7.png" alt="paper" srcset="">';
+    }else if(compickedRandom == 'pickedScissor'){
+        comDisplay.innerHTML = '<img src="Images/Group 8.png" alt="scissor" srcset="">';
+    }
